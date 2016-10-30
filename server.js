012c8eeeafafa36aaadff33f8f3a052e33bf3868
shawkes <shawkes@gmail.com>
2016-10-30 13:53:49 +0000

    woooooooooooooooooooooooooork

--- server.js
+++ server.js
@@ -407,7 +407,7 @@ router.put('/relatefacetomugshot', function(req, res) {
     }
   }
   session
-    .run("MATCH (ms:Mugshot {persistantface : {Persistantface}} ), (p:Photo {FilePath : {Filepath}} )  create (f:Face {faceId : {FaceId}, faceRectangletop : {FaceRectangletop}, faceRectangleleft : {FaceRectangleleft}, faceRectanglewidth : {FaceRectanglewidth}, faceRectangleheight : {FaceRectangleheight ,gender : {Gender}, age : {Age} })-[:ISIN]->(p), (fm:FaceMatch {convidence: {Convidence}})-[:CONFIDENCEOFBEING]->(ms), (fm)-[:OF]->(f)",
+    .run("MATCH (ms:Mugshot {persistantface : {Persistantface}} ), (p:Photo {FilePath : {Filepath}} )  create (f:Face {faceId : {FaceId}, faceRectangletop : {FaceRectangletop}, faceRectangleleft : {FaceRectangleleft}, faceRectanglewidth : {FaceRectanglewidth}, faceRectangleheight : {FaceRectangleheight} ,gender : {Gender}, age : {Age} })-[:ISIN]->(p), (fm:FaceMatch {convidence: {Convidence}})-[:CONFIDENCEOFBEING]->(ms), (fm)-[:OF]->(f)",
       { FaceId : faceId,
         FaceRectangletop : faceRectangle.top,
         FaceRectangleleft : faceRectangle.left,
@@ -417,7 +417,7 @@ router.put('/relatefacetomugshot', function(req, res) {
         Persistantface : persistantface,
         Filepath :  filepath,
         Gender: gender,
-        Age, age))
+        Age: age})
     .then(function(result){
       result.records.forEach(function(record) {
         console.log(record._fields);
